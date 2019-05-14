4fe69d7ea9fa8fdf564b232222a5a76b4b587b54
Pavel Parshakov <pavelmp@gmail.com>
2019-05-14 14:00:48 +0300

    Add SignIn, SignUp, Place components and first API call

--- src/App.js
+++ src/App.js
@@ -1,26 +1,108 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const SignUp = function(props){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Sign Up</h1>
+      <input type='text' placeholder='username' value={props.username} onChange={event => props.onChange(event)} name="username"/>
+      <input type='text' placeholder='password' value={props.password} onChange={event => props.onChange(event)} name="password"/>
+      <button onClick={() => props.signUp()}>Sign Up</button>
     </div>
   );
+};
+
+const SignIn = function(props){
+  return (
+    <div>
+      <h1>Sign In</h1>
+      <input type='text' placeholder='username' value={props.username} onChange={event => props.onChange(event)} name="username"/>
+      <input type='text' placeholder='password' value={props.password} onChange={event => props.onChange(event)} name="password"/>
+      <button onClick={() => props.signIn()}>Sign In</button>
+    </div>
+  );
+};
+
+//props.places - array with place objects {location: 'City Mall', distance: 500}
+const Places = function(props){
+  return (
+    <div>
+      <h1>Places</h1>
+      {
+        props.places.map((place, index) => {
+          return (<p key={index}>{index + 1}. {place.location} - {place.distance}</p>)
+        })
+      }
+    </div>
+  );
+};
+
+
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      username: '',
+      password: '',
+      whatPageToShow: 'signUp',
+      places: [{location: 'City Mall', distance: 500}]
+    }
+  }
+
+  componentDidMount(){
+    console.log('Called componentDidMount')
+  }
+
+  componentWillMount(){
+    console.log('Called componentWillMount')
+  }
+
+  onChange(event){
+    const name = event.target.name;
+    const value = event.target.value;
+    this.setState({[name]: value});
+  }
+
+  signUp(){
+    //Call API to sign up with username and password
+    const body = {username: this.state.username, password: this.state.password};
+    fetch('http://localhost:5000/signup', {
+      method: 'post',
+      body: JSON.stringify(body),
+      headers: {"Content-Type": "application/json"}
+    }).then(function(response) {
+      return response.text();
+    }).then(function(data) {
+      console.log(data);
+    });
+
+
+    // this.setState({whatPageToShow: 'signIn', username: '', password: ''})
+  }
+
+  signIn(){
+    //Call API to sign in with username and password
+    this.setState({whatPageToShow: 'places', username: '', password: ''})
+  }
+  
+  render(){
+    console.log('Rendered');
+    return (
+      <div className="App">
+        <header className="App-header">
+          { this.state.whatPageToShow === 'signUp'
+            ? <SignUp username={this.state.username} password={this.state.password} onChange={event => this.onChange(event)} signUp={() => this.signUp()}/>
+            : this.state.whatPageToShow === 'signIn'
+              ? <SignIn username={this.state.username} password={this.state.password} onChange={event => this.onChange(event)} signIn={() => this.signIn()}/>
+              : this.state.whatPageToShow === 'places'
+                ? <Places places={this.state.places}/>
+                : null
+          }
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
